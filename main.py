b98061aea6ead5e7b5c339500623735e6ce6431f
Sami Barchid <sami.barchid@univ-lille.fr>
2022-02-18 15:52:02 +0100

    fix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--lr', default=0.001, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
+    parser.add_argument('--epochs', default=400, type=int)
     parser.add_argument('--experiment', type=str, required=True)
     parser.add_argument('--dataset', type=str,
                         choices=['n-mnist', 'cifar10-dvs', 'dvsgesture'])
